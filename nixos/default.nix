8ad65308652c3f2d7fdb98bb6d26b003f3994a57
PierreBorine <pierre.borine@protonmail.com>
2025-04-13 00:00:25 +0200

    fix: always import Home Manager module with the NixOS module

--- nixos/default.nix
+++ nixos/default.nix
@@ -9,30 +9,33 @@ in {
     enable = lib.mkEnableOption "My Hyprland based desktop environment";
   };
 
-  config = lib.mkIf cfg.enable {
-    programs.hyprland.enable = true;
-    security.pam.services.hyprlock = {};
+  config = lib.mkMerge [
+    {home-manager.sharedModules = [self.homeManagerModules.default];}
 
-    programs.dconf.enable = true;
+    (lib.mkIf cfg.enable {
+      programs.hyprland.enable = true;
+      security.pam.services.hyprlock = {};
 
-    home-manager.sharedModules = [
-      self.homeManagerModules.default
-      {azuride.enable = lib.mkDefault true;}
-    ];
+      programs.dconf.enable = true;
 
-    nix.settings = {
-      experimental-features = ["nix-command" "flakes" "pipe-operators"];
-      builders-use-substitutes = true;
-      substituters = [
-        "https://pierreborine.cachix.org"
-        "https://ags.cachix.org"
-        "https://anyrun.cachix.org"
+      home-manager.sharedModules = [
+        {azuride.enable = lib.mkDefault true;}
       ];
-      trusted-public-keys = [
-        "pierreborine.cachix.org-1:D6WNCFqd5FZkTMem+QF+q25/lU2KFf8C7zBvvzhZZAk="
-        "ags.cachix.org-1:naAvMrz0CuYqeyGNyLgE010iUiuf/qx6kYrUv3NwAJ8="
-        "anyrun.cachix.org-1:pqBobmOjI7nKlsUMV25u9QHa9btJK65/C8vnO3p346s="
-      ];
-    };
-  };
+
+      nix.settings = {
+        experimental-features = ["nix-command" "flakes" "pipe-operators"];
+        builders-use-substitutes = true;
+        substituters = [
+          "https://pierreborine.cachix.org"
+          "https://ags.cachix.org"
+          "https://anyrun.cachix.org"
+        ];
+        trusted-public-keys = [
+          "pierreborine.cachix.org-1:D6WNCFqd5FZkTMem+QF+q25/lU2KFf8C7zBvvzhZZAk="
+          "ags.cachix.org-1:naAvMrz0CuYqeyGNyLgE010iUiuf/qx6kYrUv3NwAJ8="
+          "anyrun.cachix.org-1:pqBobmOjI7nKlsUMV25u9QHa9btJK65/C8vnO3p346s="
+        ];
+      };
+    })
+  ];
 }
