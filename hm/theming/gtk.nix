6bb7f59891045a316fd5876db9e7f087cb07402a
PierreBorine <pierre.borine@protonmail.com>
2025-04-25 23:16:14 +0200

    fix: added missing module config enables

--- hm/theming/gtk.nix
+++ hm/theming/gtk.nix
@@ -1,9 +1,11 @@
-{pkgs, lib, ...}: {
-  gtk = {
-    enable = true;
-    theme = {
-      package = lib.mkDefault pkgs.adw-gtk3;
-      name = lib.mkDefault "adw-gtk3";
+{config, pkgs, lib, ...}: {
+  config = lib.mkIf config.azuride.enable {
+    gtk = {
+      enable = true;
+      theme = {
+        package = lib.mkDefault pkgs.adw-gtk3;
+        name = lib.mkDefault "adw-gtk3";
+      };
     };
   };
 }
--- hm/theming/stylix.nix
+++ hm/theming/stylix.nix
@@ -4,7 +4,7 @@
   ...
 }: {
   # Disable some stylix modules so they don't interfere
-  config = lib.mkIf (builtins.hasAttr "stylix" config) {
+  config = lib.mkIf (config.azuride.enable && (builtins.hasAttr "stylix" config)) {
     stylix.targets.hyprland.enable = false;
     stylix.targets.hyprlock.enable = false;
     stylix.targets.kde.enable = false;
