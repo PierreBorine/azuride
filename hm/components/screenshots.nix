b553159f61e70c312ee486f9898584aeaf244656
PierreBorine <pierre.borine@protonmail.com>
2025-08-20 19:29:22 +0200

    feat: add flameshot as main screenshot utility

--- hm/components/screenshots.nix
+++ hm/components/screenshots.nix
@@ -3,29 +3,48 @@
   pkgs,
   lib,
   ...
-}: {
+}: let
+  date_format = "%d-%m-%Y_%H-%M-%S";
+in {
   config = lib.mkIf config.azuride.enable {
+    services.flameshot = {
+      enable = true;
+      settings = {
+        General = {
+          contrastOpacity = 153; # 60%
+          uiColor = "#55B5DB";
+          contrastUiColor = "#8FB64D";
+
+          filenamePattern = date_format;
+          savePath = "${config.xdg.userDirs.pictures}/Screenshots";
+
+          useGrimAdapter = true; # Wayland
+
+          showDesktopNotification = false;
+          showHelp = true; # Might disable later
+          showStartupLaunchMessage = false;
+          disabledTrayIcon = true;
+          # disable some buttons
+          buttons = "@Variant(\\0\\0\\0\\x7f\\0\\0\\0\\vQList<int>\\0\\0\\0\\0\\x12\\0\\0\\0\\0\\0\\0\\0\\x1\\0\\0\\0\\x2\\0\\0\\0\\x3\\0\\0\\0\\x4\\0\\0\\0\\x5\\0\\0\\0\\x6\\0\\0\\0\\x12\\0\\0\\0\\xf\\0\\0\\0\\x16\\0\\0\\0\\x13\\0\\0\\0\\t\\0\\0\\0\\x10\\0\\0\\0\\n\\0\\0\\0\\v\\0\\0\\0\\x17\\0\\0\\0\\f\\0\\0\\0\\x11)";
+        };
+      };
+    };
+
     home.packages = with pkgs; [
       hyprpicker
       hyprshot
-      satty
+      grim
     ];
 
-    xdg.configFile."satty/config.toml".text = ''
-      [general]
-      fullscreen = true
-      early-exit = true
-      initial-tool = "crop"
-      copy-command = "wl-copy"
-      annotation-size-factor = 1.2
-      save-after-copy = true
-    '';
-
-    wayland.windowManager.hyprland.settings = let
-      date_format = "%d-%m-%Y_%H:%M:%S";
-    in {
-      layerrule = ["noanim, selection"];
-      "$hyprshotBase" = builtins.concatStringsSep " " [
+    wayland.windowManager.hyprland.settings = {
+      windowrule = [
+        "move 0 0, title:(flameshot)"
+        "pin, title:(flameshot)"
+        "fullscreenstate, title:(flameshot)"
+        "float, title:(flameshot)"
+        "animation slide top, title:(flameshot)"
+      ];
+      "$hyprshot" = builtins.concatStringsSep " " [
         "hyprshot"
         "--silent"
         "--output-folder"
@@ -34,22 +53,13 @@
         "$(date '+${date_format}.png')"
         "--mode"
       ];
-      "$hyprshotSatty" = builtins.concatStringsSep " " [
-        "--raw"
-        "|"
-        "satty"
-        "--filename"
-        "-"
-        "--output-filename"
-        "'$XDG_PICTURES_DIR/Screenshots/${date_format}-edited.png'"
-      ];
       bind = [
-        "ALT, PRINT, exec, $hyprshotBase region --freeze"
-        "$mainMod, PRINT, exec, $hyprshotBase window --freeze"
-        ", PRINT, exec, $hyprshotBase output --mode active"
-        "ALT&SHIFT, PRINT, exec, $hyprshotBase region --freeze $hyprshotSatty"
-        "$mainMod&SHIFT, PRINT, exec, $hyprshotBase window --freeze $hyprshotSatty"
-        "SHIFT, PRINT, exec, $hyprshotBase output --mode active $hyprshotSatty"
+        # default
+        ", PRINT, exec, flameshot gui"
+
+        # quick
+        "ALT, PRINT, exec, $hyprshot region --freeze"
+        "$mainMod, PRINT, exec, $hyprshot window --freeze"
       ];
     };
   };
